bfc0e041eadabd2b0d24b01c8eee4e722a087c80
Jens Klenke <jens-klenke@hotmail.de>
2020-03-23 20:00:20 +0100

    Lag function works with gaps

--- 01_code/lag_function.R
+++ 01_code/lag_function.R
@@ -1,17 +1,28 @@
 # https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/PkgUtils
 
 
-lags <- 2
-df <- matrix(seq(1:20), ncol = 2)
+lags <- c(1,2,5)
+df <- data.frame(
+    x = c(11:20),
+    y = c(21:30))
 
-x <- matrix(NA, nrow = nrow(df), ncol =  lags) 
+x <- matrix(NA, nrow = nrow(df), ncol =  length(lags)) 
+x_names <- rep(NA, length(lags))
 X <- c()
+X_names <- NULL
 
 for (j in 1:ncol(df)) {
-    for (i in 1:lags) {
-    x[,i] <- Hmisc::Lag(df[,j], i)    
+    for (i in seq_along(lags)) {
+    x[,i] <- Hmisc::Lag(df[,j], lags[i]) 
+    x_names[i] <- paste0(names(df)[j],"_L_", lags[i] )
     }
-    X <- cbind(X,x)    
-}
+    X <- cbind(X,x)
+    X_names <- cbind(X_names, x_names)
+    colnames(X) <- X_names
+    }
  
 X
+
+
+
+
