efc30e52ac298f43f3be60545277d1ff9d99cac7
Janine-Langerbein <janine.langerbein@icloud.com>
2020-03-23 18:58:57 +0100

    größeres Beispiel lag function

--- 01_code/lag_function.R
+++ 01_code/lag_function.R
@@ -4,14 +4,23 @@
 lags <- 2
 df <- matrix(seq(1:20), ncol = 2)
 
-x <- matrix(NA, nrow = nrow(df), ncol =  lags) 
+x <- matrix(NA, nrow = nrow(df), ncol =  lags)
 X <- c()
 
 for (j in 1:ncol(df)) {
     for (i in 1:lags) {
-    x[,i] <- Hmisc::Lag(df[,j], i)    
+    x[, i] <- Hmisc::Lag(df[, j], i)    
     }
     X <- cbind(X,x)    
 }
- 
+
+# 50 Lags und 6 Variablen
+lags <- 50
+df <- matrix(seq(1:120), ncol = 6)
+df
+
+options(max.print = 10000)
 X
+
+
+
