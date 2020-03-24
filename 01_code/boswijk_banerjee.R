58671110ff85f8e8cbcbd2b8a2a2c23ef8f9a992
Janine-Langerbein <janine.langerbein@icloud.com>
2020-03-24 19:31:48 +0100

    Boswijk mit lagged values

--- 01_code/boswijk_banerjee.R
+++ 01_code/boswijk_banerjee.R
@@ -1,8 +1,24 @@
 ### Local Parameters
 lags <- c(1, 2, 5)
 
-Xlag <- cbind(q, r)
-Y_dif <- diff(q)
-W <- diff(r)
+df <- data.frame(
+    x = c(11:20),
+    y = c(21:30))
+
+# Xlag <- cbind(q, r)
+Xlag <- df
+Y_dif <- diff(q) # Problem: 0 Variablen, muss wohl numeric werden (lapply?)
+W <- diff(r) # Problem: 0 Variablen, muss wohl numeric werden (lapply?)
 
 ### Lagged values
+Xlag_diff <- as.data.frame(diff(as.matrix(Xlag)))
+
+x <- matrix(NA, nrow = nrow(Xlag_diff), ncol = length(lags))
+X <- c()
+
+for (j in 1:ncol(Xlag_diff)) {
+    for (i in seq_along(lags)) {
+        x[, i] <- Hmisc::Lag(Xlag_diff[, j], lags[i])
+    }
+    X <- cbind(X, x)
+}
