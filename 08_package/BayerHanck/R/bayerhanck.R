eb84b196f0ab8132f3d6d1e1f21e4d9b8ff1ee31
Janine-Langerbein <janine.langerbein@icloud.com>
2020-03-26 15:26:39 +0100

    Formula

--- 08_package/BayerHanck/R/bayerhanck.R
+++ 08_package/BayerHanck/R/bayerhanck.R
@@ -1,4 +1,5 @@
-bayerhanck <- function() {
+bayerhanck <- function(formula, data, trend, lags, test, crit) {
+  mf <- model.frame(formula)
 
   # Engle Granger Test
 
@@ -7,3 +8,13 @@ bayerhanck <- function() {
   # Boswijk/Banerjee Test
 
 }
+
+
+y <- 1:4
+x <- cbind(5:8, 10:13)
+z <- cbind(1:4, 20:23)
+
+form <- y ~ x + z
+mf <- model.frame(form)
+y_mf <- model.response(mf)
+
