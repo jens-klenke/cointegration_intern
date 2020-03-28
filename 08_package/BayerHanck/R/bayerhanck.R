3b56837d464b551e67c5a82b919fbc879fbc7927
Janine-Langerbein <janine.langerbein@icloud.com>
2020-03-28 10:59:40 +0100

    show only values for chosen tests

--- 08_package/BayerHanck/R/bayerhanck.R
+++ 08_package/BayerHanck/R/bayerhanck.R
@@ -39,7 +39,7 @@ bayerhanck <- function(formula, data, trend = "const", lags = 1, test = "all", c
                         johansen(formula = formula, data = data, lags = lags, trend = trend)[[1]],
                         banerjee(formula = formula, data = data, lags = lags, trend = trend)[[1]],
                         boswijk(formula = formula, data = data, lags = lags, trend = trend))[[1]]
-
+  test.stat <- test.stat[complete.cases(test.stat)]
   #-----------------------------------------------------------------------------------------
   # Obtain P-Values
   #-----------------------------------------------------------------------------------------
@@ -47,20 +47,20 @@ bayerhanck <- function(formula, data, trend = "const", lags = 1, test = "all", c
   crit_val <- rmatio::read.mat(here::here("/critical_values.mat"))
 
   pval <- test.stat
-  basecase <- 44 * (trendtype - 1) + 4 * (nvar - 2)
+  #basecase <- 44 * (trendtype - 1) + 4 * (nvar - 2)
 
-  for (i in 1:4) {
-    case = basecase + i
-    if (i %in% c(1, 3)) {
-      n <- sum(stat[i] > null_dist[, case])
-      pval[i] <-  (n/N) + .000000000001
-    } else {
-      if (i %in% c(2, 4)) {
-        n <- sum(stat[i] < null_dist[, case])
-        pval[i] <-  (n/N) + .000000000001
-      }
-    }
-  }
+  #for (i in 1:4) {
+  #  case = basecase + i
+  #  if (i %in% c(1, 3)) {
+  #    n <- sum(stat[i] > null_dist[, case])
+  #    pval[i] <-  (n/N) + .000000000001
+  #  } else {
+  #    if (i %in% c(2, 4)) {
+  #      n <- sum(stat[i] < null_dist[, case])
+  #      pval[i] <-  (n/N) + .000000000001
+  #    }
+  #  }
+  #}
 
   #-----------------------------------------------------------------------------------------
   # Calculate Bayer-Hanck Fisher Statistics
@@ -73,7 +73,6 @@ bayerhanck <- function(formula, data, trend = "const", lags = 1, test = "all", c
        p.val = pval)
 
 }
-
 
 bayerhanck(linvestment ~ lincome + lconsumption, data = df, test = c("englegranger",
                                                                            "banerjee"))
