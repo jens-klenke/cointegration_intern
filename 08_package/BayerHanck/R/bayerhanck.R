875bf497ba5d456e3685f2ecf1fb60d426477666
Janine-Langerbein <janine.langerbein@icloud.com>
2020-04-02 12:30:36 +0200

    .

--- 08_package/BayerHanck/R/bayerhanck.R
+++ 08_package/BayerHanck/R/bayerhanck.R
@@ -92,11 +92,11 @@ bayerhanck <- function(formula, data, lags = 1, trend = "const", test = "all", c
   for (i in 1:4) {
     case = basecase + i
     if (i %in% c(1, 3)) {
-      n <- sum(stat[i] > null_dist[, case])
+      n <- sum(pval.stat[i] > null_dist[, case])
       pval.stat[i] <-  (n/N) + .000000000001
     } else {
       if (i %in% c(2, 4)) {
-        n <- sum(stat[i] < null_dist[, case])
+        n <- sum(pval.stat[i] < null_dist[, case])
         pval.stat[i] <-  (n/N) + .000000000001
       }
     }
@@ -151,5 +151,4 @@ bayerhanck <- function(formula, data, lags = 1, trend = "const", test = "all", c
        p.val = pval)
   print(pval.stat)
 }
-
 
