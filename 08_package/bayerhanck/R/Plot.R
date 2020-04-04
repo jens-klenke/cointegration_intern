d9c280818b027ca600589f20e1c4bed3caa3afdc
Janine-Langerbein <janine.langerbein@icloud.com>
2020-04-04 18:35:13 +0200

    add dependency dplyr

--- 08_package/bayerhanck/R/Plot.R
+++ 08_package/bayerhanck/R/Plot.R
@@ -1,25 +1,24 @@
-plot.bh.test <- function(obj) {
+plot.bh.test <- function(object) {
 
   # Obtain Parameters from the Bayerhanck object
 
-  K <- obj$K
-  if (identical(obj$trend, "none"))
+  K <- object$K
+  if (identical(object$trend, "none"))
     trendtype <- 1
-  if (identical(obj$trend, "const"))
+  if (identical(object$trend, "const"))
     trendtype <- 2
-  if (identical(obj$trend, "trend"))
+  if (identical(object$trend, "trend"))
     trendtype <- 3
 
-  bh.stat <- obj$bh.test
-  stat.type <- obj$test.typ
-  crit <- obj$crit
+  bh.stat <- object$bh.test
+  stat.type <- object$test.typ
+  crit <- object$crit
 
 
-
-  basecase <- 44 * (trendtype - 1) + 4 * (nvar - 2)
+  basecase <- 44 * (trendtype - 1) + 4 * (object$nvar - 2)
 
   load("null_dist.rda")
-  i <- basecase
+  i <- object$basecase
 
   df_gg <- null_dist%>%
     dplyr::select(as.name(paste0('var', i)))%>%
@@ -30,7 +29,7 @@ plot.bh.test <- function(obj) {
     stat_ecdf(geom = "step")+
     geom_vline(xintercept = bh.stat, linetype = "dashed",
                color = "red", size = 1)+
-    annotate("text", x = (bh.stat*1.05), y = 0.5, label = paste('B-H-S \n', round( bh.stat,2)) ,
+    annotate("text", x = (bh.stat*1.05), y = 0.5, label = paste('B-H-S \n', round( bh.stat,2)),
              colour = 'red')+
     labs( x = "\n \n Bayer-Hanck-Statistic", y = "F(Bayer-Hanck-Statistic) \n",
           title = 'Empirical Cumulative Distribution Function')+
