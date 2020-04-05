aa96e1379cd95bac0bbd0a7362b8e772bdf5b8c8
Janine-Langerbein <janine.langerbein@icloud.com>
2020-04-05 15:43:28 +0200

    Revert "PLOTPLOTPLOT :angry:"
    This reverts commit a67999a0ab9b3553510f1f423c37494cf68cacf2.

--- 08_package/bayerhanck/R/Plot.R
+++ 08_package/bayerhanck/R/Plot.R
@@ -28,25 +28,16 @@ plot.bh.test <- function(object, theme = "dark") {
   gg.bh <- ggplot2::ggplot(data = df_gg, ggplot2::aes(x = x)) +
     ggplot2::geom_vline(xintercept = object$bh.test, linetype = "dotted",
                color = "red", size = 1) +
-    ggplot2::labs(x = "\n Bayer-Hanck-Statistic", y = "F(Bayer-Hanck-Statistic) \n") +
-    ggplot2::theme(legend.position="right") #+
-  #  ggplot2::annotate("text", x = (object$bh.test*(-1.1)), y = 0.5,
-  #                    label = paste('B-H-S \n', round(object$bh.test, 2)),
-  #                    colour = 'red')
+    ggplot2::annotate("text", x = (object$bh.test*1.05), y = 0.5,
+                      label = paste('B-H-S \n', round(object$bh.test, 2)),
+                      colour = 'red') +
+    ggplot2::labs(x = "\n \n Bayer-Hanck-Statistic", y = "F(Bayer-Hanck-Statistic) \n") +
+    ggplot2::theme(plot.margin = ggplot2::unit(c(1, 1, 1, 1),"cm"),
+          plot.title = ggplot2::element_text(hjust = 0.5))
 
   if (identical(theme, "light")) {
     gg.bh +
-      ggthemes::theme_economist() +
       ggplot2::stat_ecdf(geom = "step") +
-<<<<<<< HEAD
-      ggplot2::theme(panel.grid.major.y = ggplot2::element_line(size = 0.3, colour = "#546069"),
-                     plot.background = ggplot2::element_rect(fill = "#FFFFFF", colour = "#FFFFFF"),
-                     panel.background = ggplot2::element_rect(fill = "#FFFFFF"))
-  } else if (identical(theme, "dark")) {
-    gg.bh +
-      ggplot2::stat_ecdf(geom = "step", color = "white") +
-      ggthemes::theme_economist() +
-=======
       ggplot2::theme_minimal() +
       ggplot2::theme(panel.grid.major.y = ggplot2::element_line(size = 0.3),
                      panel.grid.minor.y = ggplot2::element_blank(),
@@ -56,23 +47,15 @@ plot.bh.test <- function(object, theme = "dark") {
   } else if (identical(theme, "dark")) {
     gg.bh +
       ggplot2::stat_ecdf(geom = "step", color = "white") +
->>>>>>> 5f0d74def01172f99a67cd856df3eb83eb377670
       ggplot2::theme(plot.background = ggplot2::element_rect(fill = "#1B2B37", colour = "#1B2B37"),
                      panel.background = ggplot2::element_rect(fill = "#1B2B37"),
                      panel.grid.major.y = ggplot2::element_line(size = 0.3, colour = "#546069"),
                      panel.grid.minor.y = ggplot2::element_blank(),
-<<<<<<< HEAD
-                     axis.text = ggplot2::element_text(colour = "#FFFFFF"),
-                     axis.line = ggplot2::element_line(size = 0.3, colour = "#546069"),
-                     axis.title = ggplot2::element_text(colour = "#FFFFFF"),
-                     axis.ticks.x = ggplot2::element_line(color = "#FFFFFF"))
-=======
                      panel.grid.major.x = ggplot2::element_line(size = 0.3, colour = "#546069"),
                      panel.grid.minor.x = ggplot2::element_blank(),
                      axis.text = ggplot2::element_text(colour = "#FFFFFF"),
                      axis.line = ggplot2::element_line(size = 0.3, colour = "#546069"),
                      axis.title = ggplot2::element_text(colour = "#FFFFFF"))
->>>>>>> 5f0d74def01172f99a67cd856df3eb83eb377670
   }
 
 }
