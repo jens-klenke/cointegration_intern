801f38f2ff6a91faa47308cf7fafa0c35e454c1c
Janine-Langerbein <janine.langerbein@icloud.com>
2020-04-05 15:57:13 +0200

    ggplot hat keine freunde

--- 08_package/bayerhanck/R/Plot.R
+++ 08_package/bayerhanck/R/Plot.R
@@ -8,22 +8,18 @@ plot.bh.test <- function(object, theme = "dark") {
   if (identical(object$test.type, "all"))
     cv <- Null_Distr_B_ECR_J_E
 
-  if (identical(object$trend, "none")){
+  if (identical(object$trend, "none")) {
     trendtype = 1
-  } else if (identical(object$trend, "const")){
+  } else if (identical(object$trend, "const")) {
     trendtype = 2
-  } else if (identical(object$trend, "trend")){
+  } else if (identical(object$trend, "trend")) {
     trendtype = 3
   }
 
-
-
-
-  x <- cv[object$K, trendtype,]
+  x <- cv[object$K, trendtype, ]
   y <- rep((1/length(x)), length(x))
 
-  df_gg <- data.frame(x,y)
-
+  df_gg <- data.frame(x, y)
 
   gg.bh <- ggplot2::ggplot(data = df_gg, ggplot2::aes(x = x)) +
     ggplot2::geom_vline(xintercept = object$bh.test, linetype = "dotted",
@@ -37,26 +33,23 @@ plot.bh.test <- function(object, theme = "dark") {
 
   if (identical(theme, "light")) {
     gg.bh +
+      ggthemes::theme_economist() +
       ggplot2::stat_ecdf(geom = "step") +
-      ggplot2::theme_minimal() +
-      ggplot2::theme(panel.grid.major.y = ggplot2::element_line(size = 0.3),
-                     panel.grid.minor.y = ggplot2::element_blank(),
-                     panel.grid.major.x = ggplot2::element_line(size = 0.3),
-                     panel.grid.minor.x = ggplot2::element_blank(),
-                     axis.line = ggplot2::element_line(size = 0.3))
+      ggplot2::theme(plot.background = ggplot2::element_rect(fill = "#FFFFFF", colour = "#FFFFFF"),
+                     panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
+                     panel.grid.major.y = ggplot2::element_line(size = 0.3, colour = "#546069"),
+                     axis.line = ggplot2::element_line(size = 0.3, colour = "#546069"))
   } else if (identical(theme, "dark")) {
     gg.bh +
+      ggthemes::theme_economist() +
       ggplot2::stat_ecdf(geom = "step", color = "white") +
       ggplot2::theme(plot.background = ggplot2::element_rect(fill = "#1B2B37", colour = "#1B2B37"),
                      panel.background = ggplot2::element_rect(fill = "#1B2B37"),
                      panel.grid.major.y = ggplot2::element_line(size = 0.3, colour = "#546069"),
-                     panel.grid.minor.y = ggplot2::element_blank(),
-                     panel.grid.major.x = ggplot2::element_line(size = 0.3, colour = "#546069"),
-                     panel.grid.minor.x = ggplot2::element_blank(),
                      axis.text = ggplot2::element_text(colour = "#FFFFFF"),
                      axis.line = ggplot2::element_line(size = 0.3, colour = "#546069"),
-                     axis.title = ggplot2::element_text(colour = "#FFFFFF"))
+                     axis.title = ggplot2::element_text(colour = "#FFFFFF"),
+                     axis.ticks.x = ggplot2::element_line(colour = "#FFFFFF"))
   }
-
 }
 
