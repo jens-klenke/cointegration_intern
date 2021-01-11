879f8348bcaa6fd5c0a6517c9f1391958a3e971f
Janine-Langerbein <janine.langerbein@icloud.com>
2021-01-11 20:41:04 +0100

    Update model_estimation_E_J.R

--- 09_simulation_and_approximation-cdf/model_estimation_E_J.R
+++ 09_simulation_and_approximation-cdf/model_estimation_E_J.R
@@ -1,75 +1,79 @@
-### functions  ####
+#-----------------------
+# Functions
+#-----------------------
 metric_fun <- function(object){
-    model <- substitute(object)
-    
-    values <- tibble(PRED = object$fitted.values,
-                        dependent = object$model[,'p_value_Fisher_E_J'])%>%
+    values <- tibble(
+        PRED = object$fitted.values,
+        dependent = object$model[, 'p_value_Fisher_E_J']) %>%
         dplyr::mutate(PRED_cor = case_when(
             PRED <= 0 ~ 1e-12,
             PRED >= 1 ~ 1-1e-12,
             TRUE ~ PRED))
     
-    
     RMSE <- sqrt((sum((values$PRED - values$dependent)^2)/nrow(values)))
     RMSE_cor <- sqrt((sum((values$PRED_cor - values$dependent)^2)/nrow(values)))
-    
     values_0.2 <- values %>%
         dplyr::filter(dependent >= 0.8)
-    
     RMSE_0.2 <- sqrt((sum((values_0.2$PRED - values_0.2$dependent)^2)/nrow(values_0.2)))
     RMSE_cor_0.2 <- sqrt((sum((values_0.2$PRED_cor - values_0.2$dependent)^2)/nrow(values_0.2)))
     
     mod_call <- object$call$form[3]
     case <- object$call$data
     
-    metric <- c(as.character(model), as.numeric(RMSE), as.numeric(RMSE_cor), 
-                as.numeric(RMSE_0.2), as.numeric(RMSE_cor_0.2), as.character(mod_call), as.character(case))
-    
-    return(metric)
+    c(as.character(mod_call), as.numeric(RMSE), as.numeric(RMSE_cor), 
+      as.numeric(RMSE_0.2), as.numeric(RMSE_cor_0.2), as.character(case))
 }
 
-# delete function 
+bind_model_metrics <- function(object) {
+    model_metrics_E_J <- rbind(model_metrics_E_J, 
+                               metric_fun(object))
+    model_metrics_E_J
+}
 
+# Delete Function 
 delete_fun <- function(){
     rm(list=setdiff(ls(.GlobalEnv), important_things), envir = .GlobalEnv)
 }
 
-#### packages ####
+
+#-----------------------
+# Preliminary 
+#-----------------------
 source(here::here('01_code/packages/packages.R'))
 
-## load simulation data 
-
-if(Sys.info()['nodename'] == "DESKTOP-ATT92OH"){ # jens 
+## Load Simulation Data 
+if(Sys.info()['nodename'] == "DESKTOP-ATT92OH") { # Jens 
     Data <- readRDS('C:\\Users\\Jens-\\Dropbox\\jens\\BayerHanck\\Data_100k.rds')
-#    load('C:\\Users\\Jens-\\Dropbox\\jens\\BayerHanck\\Data_1_m.RData')
-} else if(Sys.info()['nodename'] == "OEK-TS01"){ # Server
+    # load('C:\\Users\\Jens-\\Dropbox\\jens\\BayerHanck\\Data_1_m.RData')
+} else if(Sys.info()['nodename'] == "OEK-TS01") { # Server
     load('D:\\Klenke\\Data_1_m.RData')
 }
 
-# split dataset in cases
+# Split Dataset in Cases
 data_case_1 <- Data %>%
     dplyr::filter(case == 1)
-
 data_case_2 <- Data %>%
     dplyr::filter(case == 2)
-
 data_case_3 <- Data %>%
     dplyr::filter(case == 3)
 
-### set up metrics
+
+#-----------------------
+# Set up metrics
+#-----------------------
 model_metrics_E_J <- NULL
 
 # save important things
-important_things <- ls()
-# add the list of important things to things
-important_things <- ls()
+important_things <- c(ls(), 
+                      "important_things")
 
+
+#-----------------------
+# Models - Case 1
+#-----------------------
 tictoc::tic()
 
-#### models  case 1 ####
 # functional form: poly(t, p) + (1/k)
-
-
 mod_E_J_case.1_p_3 <- lm(p_value_Fisher_E_J ~ poly(stat_Fisher_E_J, 3) + I(1/k),
                                    data = data_case_1)
 
@@ -119,7 +123,7 @@ model_metrics_E_J <- rbind(model_metrics_E_J ,
 # delete model
 delete_fun()
 
-####  chi^p ln  
+# functional form: poly(t, p) + log(k) + poly(t, p)*log(k)
 mod_E_J_case.1_p_3_log <- lm(p_value_Fisher_E_J ~ poly(stat_Fisher_E_J, 3) + log(k) + poly(k, 3)*log(k),
                                        data = data_case_1)
 
@@ -142,7 +146,7 @@ model_metrics_E_J <- rbind(model_metrics_E_J ,
 # delete model
 delete_fun()
 
-####  chi^p + k + (1/k)  
+# functional form: poly(t, p) + k + (1/k)
 mod_E_J_case.1_p_3_k_1 <- lm(p_value_Fisher_E_J ~ poly(stat_Fisher_E_J, 3) + k + I(1/k),
                                        data = data_case_1)
 
@@ -162,9 +166,42 @@ model_metrics_E_J <- rbind(model_metrics_E_J ,
                        metric_fun(mod_E_J_case.1_p_5_k_1),
                        metric_fun(mod_E_J_case.1_p_6_k_1))
 
+
+
+
 # delete model
 delete_fun()
 
+# log()
+models_log <- list(
+    mod_E_J_case.1_log_1 = lm(p_value_Fisher_E_J ~ log(stat_Fisher_E_J) + k,
+                             data = data_case_1),
+    mod_E_J_case.1_log_2 = lm(p_value_Fisher_E_J ~ log(stat_Fisher_E_J) * k,
+                           data = data_case_1),
+    mod_E_J_case.1_poly_log_3 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 3) + log(k),
+                           data = data_case_1),
+    mod_E_J_case.1_poly_log_4 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 4) + log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_5 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 5) + log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_6 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 6) + log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_7 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 7) + log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_m_3 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 3) * log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_m_4 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 4) * log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_m_5 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 5) * log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_m_6 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 6)  *log(k),
+                                data = data_case_1),
+    mod_E_J_case.1_poly_log_m_7 <- lm(p_value_Fisher_E_J ~ poly(log(stat_Fisher_E_J), 7) * log(k),
+                                data = data_case_1))
+
+model_metrics <- models_log %>% purrr::map(bind_model_metrics)
+
+#------
 ### GAM 
 mod_E_G_case.1_gam_6 <- mgcv::gam(p_value_Fisher_E_J ~ poly(stat_Fisher_E_J, 6) + ns(k, 6) + ns(stat_E_G*k, 6),
                                   data = data_case_1)
@@ -176,11 +213,13 @@ model_metrics_E_J <- rbind(model_metrics_E_J ,
 delete_fun()
 tictoc::toc()
 
-############################ Case 2 ##########################
+#-----------------------
+# Models - Case 2
+#-----------------------
 
-
-############################ Case 3 ##########################
-
+#-----------------------
+# Models - Case 3
+#-----------------------
 
 
 
