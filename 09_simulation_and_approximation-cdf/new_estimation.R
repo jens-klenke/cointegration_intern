7d8db1f2cdf3ddabc38e877cc7d71b2ef524e81e
Janine-Langerbein <janine.langerbein@icloud.com>
2021-04-28 14:45:26 +0200

    save tibble lambda values

--- 09_simulation_and_approximation-cdf/new_estimation.R
+++ 09_simulation_and_approximation-cdf/new_estimation.R
@@ -63,6 +63,9 @@ lambda_values <- bind_rows(lambda_bc_all,
     dplyr::mutate(test = c(rep("all", 6), 
                            rep("e_j", 6)))
 
+#save(lambda_values, file = here::here('09_simulation_and_approximation-cdf/lambda_package.RData'))
+
+
 #-- calls and range of power ----
 # E_J 
 calls_E_J <- c('p_value_Fisher ~ poly(stat_Fisher_E_J, power)',
