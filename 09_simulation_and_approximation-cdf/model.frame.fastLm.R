08608ea0eb0a230a39e05f05760d64f894b95ba3
Janine-Langerbein <janine.langerbein@icloud.com>
2021-06-17 23:20:53 +0200

    Update model.frame.fastLm.R

--- 09_simulation_and_approximation-cdf/model.frame.fastLm.R
+++ 09_simulation_and_approximation-cdf/model.frame.fastLm.R
@@ -16,6 +16,21 @@ model.frame.fastLm <- function (formula, ...)
     if (is.null(env)) 
         env <- parent.frame()
     eval(fcall, env)
-
 }
 
+model.matrix.fastLm <- function (object, ...) {
+    if (n_match <- match("x", names(object), 0L)) 
+        object[[n_match]]
+    else {
+        data <- model.frame(object, xlev = object$xlevels, ...)
+        if (exists(".GenericCallEnv", inherits = FALSE)) 
+            NextMethod("model.matrix", data = data, contrasts.arg = object$contrasts)
+        else {
+            dots <- list(...)
+            dots$data <- dots$contrasts.arg <- NULL
+            do.call("model.matrix.default", c(list(object = object, 
+                                                   data = data, contrasts.arg = object$contrasts), 
+                                              dots))
+        }
+    }
+}
