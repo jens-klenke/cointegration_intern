c4ed5f0af1f20476515d7bfd66300e64d1367d3e
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2021-12-14 17:28:51 +0100

    added data

--- 09_simulation_and_approximation-cdf/controlling.R
+++ 09_simulation_and_approximation-cdf/controlling.R
@@ -12,14 +12,13 @@ if(Sys.info()['user'] == "Jens-"){
     load('C:\\Users\\Jens-\\Dropbox\\jens\\BayerHanck\\Data_1_m.RData')
 }
 
-
 own_sim_critical_val <- Data %>% 
     dplyr::filter(p_value_Fisher_all == 0.95)%>%
     dplyr::select(p_value_Fisher_all, stat_Fisher_all,  k, case)%>%
     dplyr::arrange(case, k)%>%
     dplyr::mutate(c_val = c(crit_val_2_0.05))%>%
     dplyr::mutate(diff = stat_Fisher_all - c_val, 
-                  diff_procent = diff/c_val * 100)
+                  diff_percent = diff/c_val * 100)
 
 save(own_sim_critical_val, file = here::here('09_simulation_and_approximation-cdf/comparison.Rds'))
 
